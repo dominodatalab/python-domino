3de5be03df6993eb86be50b6719b78bb144d3d18
macrogers <25593574+macrogers@users.noreply.github.com>
2017-11-30 17:14:09 -0500

    Update domino.py
    Added a app_publish def, need to test

--- domino/domino.py
+++ domino/domino.py
@@ -244,6 +244,11 @@ class Domino:
             raise Exception(disposition.get("message"))
         else:
             return disposition
+        
+    def app_publish(self):
+        url = self._routes.app_publish()
+        request = {"language" : "App"}
+        response = requests.post(url, auth=('', self._api_key), json=request)
 
     # Helper methods
     def _get(self, url):
