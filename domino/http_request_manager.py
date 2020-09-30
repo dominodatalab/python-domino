dcc285a0933d4307f29c26e5cf23035c791c5c4f
Abhijeet Sharma <abhijeet.sharma@dominodatalab.com>
2020-10-01 02:52:27 +0530

    [DOM-23013] Use `time_ns` instead of `time` in app_create (#76)
    * * Raising Execption for status code 4xx and 5xx
    * logging response text for these Execptions
    
    * fix publish app
    
    * Added Changelog

--- domino/http_request_manager.py
+++ domino/http_request_manager.py
@@ -1,4 +1,5 @@
 from requests.auth import AuthBase
+import logging
 import requests
 
 
@@ -9,18 +10,27 @@ class _HttpRequestManager:
     """
     def __init__(self, auth: AuthBase):
         self.auth = auth
+        self._logger = logging.getLogger(__name__)
 
     def post(self, url, data=None, json=None, **kwargs):
-        return requests.post(url, auth=self.auth, data=data, json=json, **kwargs)
+        return self._raise_for_status(requests.post(url, auth=self.auth, data=data, json=json, **kwargs))
 
     def get(self, url, **kwargs):
-        return requests.get(url, auth=self.auth, **kwargs)
+        return self._raise_for_status(requests.get(url, auth=self.auth, **kwargs))
 
     def put(self, url, data=None, **kwargs):
-        return requests.put(url, auth=self.auth, data=data, **kwargs)
+        return self._raise_for_status(requests.put(url, auth=self.auth, data=data, **kwargs))
 
     def delete(self, url, **kwargs):
-        return requests.delete(url, auth=self.auth, **kwargs)
+        return self._raise_for_status(requests.delete(url, auth=self.auth, **kwargs))
 
     def get_raw(self, url):
         return self.get(url, stream=True).raw
+
+    def _raise_for_status(self, response):
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self._logger.error(e.response.text)
+            raise
+        return response
