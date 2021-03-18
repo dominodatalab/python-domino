1c1cf86c0c61899561660aef755055b8e045d741
dominoniole <niole.nelson@dominodatalab.com>
2021-03-18 15:52:15 -0700

    Add compute cluster properties to job launching

--- domino/helpers.py
+++ domino/helpers.py
@@ -13,6 +13,17 @@ def is_version_compatible(version: str) -> bool:
     """
     return parse_version(version) >= parse_version(MINIMUM_SUPPORTED_DOMINO_VERSION)
 
+def is_cluster_type_supported(version: str, cluster_type: str) -> bool:
+    curr_version = parse_version(version)
+
+    return next(
+        (True for ct,min_version in CLUSTER_TYPE_MIN_SUPPORT if ct == cluster_type and curr_version >= parse_version(min_version)),
+        False
+    )
+
+def is_compute_cluster_properties_supported(version: str) -> bool:
+    return parse_version(version) >= parse_version(MINIMUM_DISTRIBUTED_CLUSTER_SUPPORT_DOMINO_VERSION)
+
 
 def is_on_demand_spark_cluster_supported(version: str) -> bool:
     return parse_version(version) >= parse_version(MINIMUM_ON_DEMAND_SPARK_CLUSTER_SUPPORT_DOMINO_VERSION)
