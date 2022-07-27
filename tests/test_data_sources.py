1846d89e3f1f89a91c29ead71766538a4ea9701e
Gabriel Haim <gabriel.haim@dominodatalab.com>
2022-07-26 17:09:43 -0700

    Add tests for data_sources import magic

---
+++ tests/test_data_sources.py
@@ -0,0 +1,36 @@
+import os
+import pytest
+
+
+@pytest.mark.skipif(os.getenv("DATA_SDK") != "yes", reason="Extra dependency required")
+def test_data_sources_import():
+    from domino.data_sources import DataSourceClient
+    from domino.data_sources import ADLSConfig
+
+    assert DataSourceClient
+    assert ADLSConfig
+
+
+@pytest.mark.skipif(os.getenv("DATA_SDK") != "yes", reason="Extra dependency required")
+def test_data_sources_import_cant_find():
+    try:
+        from domino.data_sources import NotADatasourceConfig  # noqa
+    except ImportError as exc:
+        assert (
+            "cannot import name 'NotADatasourceConfig' from 'domino.data_sources'"
+            in str(exc)
+        )
+
+
+@pytest.mark.skipif(
+    os.getenv("DATA_SDK") == "yes", reason="Testing failure when dependency is missing"
+)
+def test_data_sources_import_fails():
+    try:
+        from domino.data_sources import DataSourceClient  # noqa
+    except ImportError as exc:
+        assert "Please pip install dominodatalab-data" in str(exc)
+    else:
+        assert (
+            False
+        ), "Import should be failing when dominodatalab-data is not installed"
