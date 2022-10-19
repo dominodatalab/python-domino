1ea44838459979a1f051fe170745b2f4025f7642
Olson J Dimanche <olson.dimanche@dominodatalab.com>
2022-10-18 20:32:28 -0700

    DOM-41737 - updated environment and spark tests

--- tests/test_spark_operator.py
+++ tests/test_spark_operator.py
@@ -9,14 +9,21 @@ from datetime import datetime
 from airflow import DAG
 from airflow.models import TaskInstance
 import pytest
+from pprint import pformat
 
 from domino.airflow import DominoSparkOperator
 from domino.exceptions import RunFailedException
+from domino.helpers import domino_is_reachable
 
 TEST_PROJECT = os.environ.get("DOMINO_SPARK_TEST_PROJECT")
+
+
 SPARK_ENVIRONMENT_ID = os.environ.get("DOMINO_SPARK_TEST_ENVIRONMENT_ID")
 
 
+@pytest.mark.skipif(
+    not domino_is_reachable(), reason="No access to a live Domino deployment"
+)
 def test_spark_operator_no_cluster():
     execution_dt = datetime.now()
 
@@ -27,11 +34,16 @@ def test_spark_operator_no_cluster():
         project=TEST_PROJECT,
         command="test_spark.py",
     )
+    task.run()
     ti = TaskInstance(task=task, execution_date=execution_dt)
     task.execute(ti.get_template_context())
 
 
-def test_spark_operator_with_cluster():
+@pytest.mark.skipif(
+    not domino_is_reachable(), reason="No access to a live Domino deployment"
+)
+def test_spark_operator_with_cluster(spark_environment_id):
+
     execution_dt = datetime.now()
     dag = DAG(dag_id="foo", start_date=execution_dt)
     task = DominoSparkOperator(
@@ -40,14 +52,18 @@ def test_spark_operator_with_cluster():
         project=TEST_PROJECT,
         command="test_spark.py",
         on_demand_spark_cluster_properties={
-            "computeEnvironmentId": SPARK_ENVIRONMENT_ID,
+            "computeEnvironmentId": spark_environment_id,
             "executorCount": 3,
         },
     )
+    task.run()
     ti = TaskInstance(task=task, execution_date=execution_dt)
     task.execute(ti.get_template_context())
 
 
+@pytest.mark.skipif(
+    not domino_is_reachable(), reason="No access to a live Domino deployment"
+)
 def test_spark_operator_no_cluster_failed():
     execution_dt = datetime.now()
 
@@ -58,7 +74,8 @@ def test_spark_operator_no_cluster_failed():
         project=TEST_PROJECT,
         command="test_spark_fail.sh",
     )
-    ti = TaskInstance(task=task, execution_date=execution_dt)
 
     with pytest.raises(RunFailedException):
+        task.run()
+        ti = TaskInstance(task=task, execution_date=execution_dt)
         task.execute(ti.get_template_context())
