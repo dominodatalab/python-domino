7d81bf15dc4be4c17db8845b1b159d8614ad0992
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-03-20 14:20:49 +0100

    Replace bs4 with beautifulsoup4 in `install_requires` (#168)
    * Replace bs4 with beautifulsoup4.
    
    * Pin beautifulsoup to 4.
    
    * Add changelog entry.
    
    * Fix beautifulsoup4 version.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords=["Domino Data Lab", "API"],
     python_requires='>=3.7.0',
-    install_requires=["packaging", "requests>=2.4.2", "bs4==0.*,>=0.0.1", "polling2~=0.5.0",
+    install_requires=["packaging", "requests>=2.4.2", "beautifulsoup4~=4.11", "polling2~=0.5.0",
                       "urllib3~=1.26.12", "typing-extensions~=4.4.0", "frozendict~=2.3.4", "python-dateutil~=2.8.2"],
     extras_require={
         "airflow": ["apache-airflow==2.2.4"],
