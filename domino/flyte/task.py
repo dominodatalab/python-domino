055e83c7cf858621631940373bac74133d59058d
noahjax <noah.jackson@dominodatalab.com>
2024-01-19 11:26:42 -0800

    initial logic for resolving the job config

--- domino/flyte/task.py
+++ domino/flyte/task.py
@@ -1,4 +1,6 @@
 import json
+import os
+import requests
 from enum import Enum
 from typing import Any, Dict, Optional, Type, List
 from datetime import timedelta
@@ -77,19 +79,40 @@ class DominoJobConfig(object):
     ExternalVolumeMounts: Optional[List[str]] = None
 
 
+@dataclass
+class ResolvedDominoJobConfig(object):
+    ### Auth ###
+    Username: str
+    ProjectName: str
+    ApiKey: str
+    ### Job Config ###
+    Command: str
+    Title: Optional[str] = None
+    CommitId: str
+    MainRepoGitRef: Optional[GitRef] = None
+    HardwareTierId: str
+    EnvironmentId: str
+    EnvironmentRevisionSpec: EnvironmentRevisionSpec
+    ComputeClusterProperties: Optional[ClusterProperties] = None
+    VolumeSizeGiB: float
+    ExternalVolumeMounts: List[str]
+
 class DominoJobTask(AsyncAgentExecutorMixin, PythonTask[DominoJobConfig]):
     def __init__(
         self,
         name: str,
-        task_config: DominoJobConfig,
+        domino_job_config: DominoJobConfig,
         inputs: Optional[Dict[str, Type]] = None,
         outputs: Optional[Dict[str, Type]] = None,
         **kwargs,
-    ):        
+    ):       
+
+        resolved_job_config = self._resolve_job_properties(domino_job_config)
+
         super().__init__(
             name=name,
             task_type="domino_job",
-            task_config=task_config,
+            task_config=resolved_job_config,
             interface=Interface(),
             inputs=inputs,
             outputs=outputs,
@@ -98,7 +121,26 @@ class DominoJobTask(AsyncAgentExecutorMixin, PythonTask[DominoJobConfig]):
         )
 
 
-    # This is used to surface DominoJobConfig values necessary to send the request    
+    def _resolve_job_properties(self, domino_job_config: DominoJobConfig) -> ResolvedDominoJobConfig:
+        if (
+            domino_job_config.CommitId is None or
+            domino_job_config.HardwareTierId is None or
+            domino_job_config.EnvironmentId is None or
+            domino_job_config.EnvironmentRevisionSpec is None or
+            domino_job_config.VolumeSizeGiB is None or
+            domino_job_config.ExternalVolumeMounts is None
+            # TODO: Compute cluster validation
+        ):
+            url = f"{os.environ["DOMINO_API_PROXY"]}/v4/jobs/{domino_job_config.OwnerName}/{domino_job_config.ProjectName}/resolveJobDefaults"
+            response = requests.post(url, json=json.dumps(asdict(domino_job_config)))
+            response.raise_for_status()
+            
+            return DominoJobConfig(**json.loads(response.json()))
+        else:
+            return domino_job_config
+        
+
+    # This is used to surface ResolvedDominoJobConfig values necessary to send the request    
     def get_custom(self, settings: SerializationSettings) -> Dict[str, Any]:
         return {
             "jobConfig": json.dumps(asdict(self._task_config))
