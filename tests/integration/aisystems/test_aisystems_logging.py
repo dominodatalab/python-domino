558c3a90d59e0b4b423cd79d577c624a0a8760d0
niole <niolenelson@gmail.com>
2025-08-28 15:32:29 -0700

    Remove extra test

--- tests/integration/aisystems/test_aisystems_logging.py
+++ tests/integration/aisystems/test_aisystems_logging.py
@@ -41,25 +41,3 @@ def test_log_evaluation_invalid_name(setup_mlflow_tracking_server, mlflow, loggi
 			value=1,
 			name="*",
 		)
-
-def test_log_evaluation_prod(setup_mlflow_tracking_server, mlflow, logging):
-	# create experiment
-	exp = mlflow.set_experiment("test_log_evaluation_prod")
-
-	fixture_create_traces()
-
-	# log evaluations to traces
-	traces = mlflow.search_traces(experiment_ids=[exp.experiment_id], filter_string="trace.name = 'test_add'", return_type='list')
-	for trace in traces:
-		logging.log_evaluation(
-			trace.info.trace_id,
-			value=1,
-			name="helpfulness",
-		)
-
-	# verify tags on traces
-	tagged_traces = mlflow.search_traces(experiment_ids=[exp.experiment_id], filter_string="trace.name = 'test_add'", return_type="list")
-	tags = tagged_traces[0].info.tags
-
-	assert tags['domino.prog.metric.helpfulness'] == '1'
-	assert tags['domino.internal.is_eval'] == 'true'
