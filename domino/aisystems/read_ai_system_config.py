c61c296a55be15713db0af9b1406b567bd0aeb08
niole <niolenelson@gmail.com>
2025-09-18 14:02:35 -0700

    Ad doc to read ai system config

--- domino/aisystems/read_ai_system_config.py
+++ domino/aisystems/read_ai_system_config.py
@@ -22,13 +22,24 @@ def get_flattened_ai_system_config(path: Optional[str] = None) -> dict[str, any]
     return flatten_dict(config)
 
 def read_ai_system_config(path: Optional[str] = None) -> dict:
-        path = path or _get_ai_system_config_path()
-        params = {}
-        try:
-                with open(path, 'r') as f:
-                    params = yaml.safe_load(f)
-        except Exception as e:
-                logging.warning(f"Failed to read ai system config yaml at path {path}: {e}")
+    """For getting the ai_system_config.yaml file and reading it into a dictionary.
+    If no path is provided it will look for the path in the DOMINO_AI_SYSTEM_CONFIG_PATH. See environment variables
+    docs for default values.
 
-        return params
+    Args:
+        path: Location of the ai system config yaml file.
+
+    Returns:
+        A dictionary of the ai system config yaml values.
+
+    """
+    path = path or _get_ai_system_config_path()
+    params = {}
+    try:
+            with open(path, 'r') as f:
+                params = yaml.safe_load(f)
+    except Exception as e:
+            logging.warning(f"Failed to read ai system config yaml at path {path}: {e}")
+
+    return params
 
