654e189b3567ce585c03cca5f92edc7db338b00b
niole <niolenelson@gmail.com>
2025-09-23 16:26:01 -0700

    Update tests

--- domino/aisystems/tracing/_util.py
+++ domino/aisystems/tracing/_util.py
@@ -7,16 +7,13 @@ def get_is_production() -> bool:
 def _get_ai_system_id() -> str | None:
     return os.environ.get("DOMINO_APP_ID")
 
-def _get_ai_system_version() -> str | None:
-    return os.environ.get("DOMINO_APP_VERSION")
-
 def is_ai_system() -> bool:
     return get_is_production() and _get_ai_system_id() is not None
 
-def build_ai_system_experiment_name(id: str, version: str) -> str:
+def build_ai_system_experiment_name(id: str) -> str:
     return id
 
 def get_running_ai_system_experiment_name() -> str | None:
     if is_ai_system():
-        return build_ai_system_experiment_name(_get_ai_system_id(), _get_ai_system_version())
+        return build_ai_system_experiment_name(_get_ai_system_id())
     return None
