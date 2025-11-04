e146d94de44e0711f193987fe1c3c4b9168b4568
Fred Gruber <Fred.Gruber@modernatx.com>
2025-11-04 16:27:38 -0500

    try fixing python 10 incompatibility with typing

--- domino/_impl/custommetrics/api_client.py
+++ domino/_impl/custommetrics/api_client.py
@@ -1271,7 +1271,7 @@ class Api:
         self.api_client = api_client
 
     @staticmethod
-    def _verify_typed_dict_inputs_oapg(cls: typing.Type[typing_extensions.TypedDict], data: typing.Dict[str, typing.Any]):
+    def _verify_typed_dict_inputs_oapg(cls: type, data: typing.Dict[str, typing.Any]):
         """
         Ensures that:
         - required keys are present
