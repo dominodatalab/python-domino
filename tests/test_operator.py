b25c9002ce021a2caceca7558b2b0bb16e19d480
Bira Ignacio <bira.ignacio@dominodatalab.com>
2026-03-17 12:13:28 -0400

    Improvements to add_tracing function (#250)
    * feat: allow span type and attribute in domino trace spans
    
      - added span_type and attributes param to the add_tracing
      - fixed some existing unit tests

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -1,23 +1,27 @@
-"""
-To run these tests you need to have at least configured airflow in
-local mode and run:
-
-`airflow db init`
-"""
 import os
 from datetime import datetime
 
 import pytest
 from airflow.operators.dummy import DummyOperator
+from airflow import settings
+from airflow.models.base import Base
 
 from domino.airflow import DominoOperator
 from domino.exceptions import RunFailedException
+from domino.helpers import domino_is_reachable
 from airflow import DAG
 from airflow.models import TaskInstance
 
 
 TEST_PROJECT = os.environ.get("DOMINO_TEST_PROJECT")
 dag_id = "test_operator"
+
+
+@pytest.fixture(autouse=True, scope="module")
+def setup_airflow_db():
+    Base.metadata.create_all(settings.engine)
+    yield
+    Base.metadata.drop_all(settings.engine)
 
 
 def test_airflow_dags():
@@ -39,6 +43,9 @@ def test_airflow_dags():
     task.execute(ti.get_template_context())
 
 
+@pytest.mark.skipif(
+    not domino_is_reachable(), reason="No access to a live Domino deployment"
+)
 def test_operator():
     start_time = datetime.now()
 
@@ -56,6 +63,9 @@ def test_operator():
     task.execute(ti.get_template_context())
 
 
+@pytest.mark.skipif(
+    not domino_is_reachable(), reason="No access to a live Domino deployment"
+)
 def test_operator_fail(caplog):
     execution_dt = datetime.now()
 
@@ -74,6 +84,9 @@ def test_operator_fail(caplog):
         task.execute(ti.get_template_context())
 
 
+@pytest.mark.skipif(
+    not domino_is_reachable(), reason="No access to a live Domino deployment"
+)
 def test_operator_fail_invalid_tier(caplog):
     execution_dt = datetime.now()
 
